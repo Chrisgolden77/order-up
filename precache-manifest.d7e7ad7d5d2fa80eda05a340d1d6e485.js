48b3b8f8681ad9d0fd5c7ad59e7c281ee961fa7a
Chris Golden <chrgolden@gmail.com>
2020-11-04 10:20:33 -0800

    Updates

---
+++ precache-manifest.d7e7ad7d5d2fa80eda05a340d1d6e485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28083c00d4c01ff9dc4943e7223b2b4",
+    "url": "/order-up/index.html"
+  },
+  {
+    "revision": "32aa522459ea94cc895a",
+    "url": "/order-up/static/css/main.919add3a.chunk.css"
+  },
+  {
+    "revision": "d2ce92bc49e728936716",
+    "url": "/order-up/static/js/2.7345cbee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/order-up/static/js/2.7345cbee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32aa522459ea94cc895a",
+    "url": "/order-up/static/js/main.6cb51986.chunk.js"
+  },
+  {
+    "revision": "eb8b094b064a9b471e55",
+    "url": "/order-up/static/js/runtime-main.42b5af65.js"
+  }
+]);
